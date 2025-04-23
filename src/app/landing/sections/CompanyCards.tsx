8a3e3c9aaf4060fcd2c618c599153ffa92d019ae
k3vin333 <k3vinalt@gmail.com>
2025-04-24 07:59:02 +1000

    fixing merge conflicts

--- src/app/landing/sections/CompanyCards.tsx
+++ src/app/landing/sections/CompanyCards.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import Link from "next/link";
 
 type ESGData = {
   ticker: string;
